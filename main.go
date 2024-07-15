1bfc7830035c4aca5c542e411b7396a550eb86d5
ADWAITH-553 <adwaithshinod553@gmail.com>
2024-07-15 12:28:24 +0530

    variables constant and formatting output done

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	const conferenceTickets = 50 //dont change
+	var remainingTickets = 50
+	var conferenceName = "Go Conference"
+	fmt.Printf("Welcome to %v our booking app\n", conferenceName)
+	fmt.Println("we have total of ", conferenceTickets, "remaining", remainingTickets)
+	fmt.Println("Book ur tickets for conference")
+
 }
